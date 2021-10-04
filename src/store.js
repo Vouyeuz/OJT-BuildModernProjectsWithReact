34be629faba4f9a7d52ca3791ab77fbd0a3b5c26
Vouyeuz <fahrizal.indi@gmail.com>
2021-10-04 16:14:32 +0700

    exercise #1, review till redux, redux-thunk next

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { todos } from "./todos/reducers";
 
 const reducers = {
@@ -17,4 +19,5 @@ const persistConfig = {
 const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(persistedReducer);
+export const configureStore = () =>
+  createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)));
